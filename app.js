ba378d7d3a18429155e98966c6756aeb13b24f9e
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-03-01 14:59:52 -0600

    Look at using maps instead of array

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
-let myLibrary = [];
+let myLibrary = new Map();
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 
-  this.read_info = function(read) {
-    if (read == true) {
-        return 'have read'
-    } else {
-        return 'not read yet'
+    this.read_info = function (read) {
+      if (read == true) {
+        return 'have read';
+      } else {
+        return 'not read yet';
+      }
     }
-  };
+  }
 
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${this.read_info(read)}`
-  };
+  info() {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read_info(this.read)}`;
+    }
 };
+
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false)
 
 function addBookToLibrary(newBook) {
     return myLibrary.push(newBook)
-  }
+}
 
-  function deleteBookToLibrary() {
-    // pass
-  }
+function deleteBookToLibrary() {
+  // pass
+}
 
-  function displayLibrary() {
-    // pass
-  }
+function displayLibrary() {
+  // pass
+}
+
+//addBookToLibrary(theHobbit)
+
+
+console.log(theHobbit.info())
+
+myLibrary.set(`${theHobbit.title}, ${theHobbit.author}`, theHobbit)
+
+console.log(myLibrary.entries())
