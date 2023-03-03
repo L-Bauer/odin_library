0de9be3f537f8f796ea9427bf452671b513c2fa6
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-03-03 14:54:31 -0600

    Build function to display table of books

--- app.js
+++ app.js
@@ -8,19 +8,24 @@ function Book(title, author, pages, read) {
 };
 
 Book.prototype.read_info = function(read) {
-  if (read == true) {
-      return 'have read'
+  if (read) {
+      return 'Yes'
   } else {
-      return 'not read yet'
+      return 'No'
   }
 };
 
 Book.prototype.info = function () {
-  return `${title} by ${author}, ${pages} pages, ${this.read_info(read)}`
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read_info(this.read)}`
 };
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false)
+//Example books
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, 0)
+const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 310, 0)
+const greatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 275, 1)
+const mobyDick = new Book("Moby Dick", "Herman Melville", 125, 0)
 
+//Library Functions
 function addBookToLibrary(newBook) {
     return myLibrary.push(newBook)
 }
@@ -29,11 +34,24 @@ function deleteBookToLibrary() {
   // pass
 }
 
+// https://stackoverflow.com/questions/72527457/how-to-create-an-html-table-from-an-array-of-objects
+// https://stackoverflow.com/questions/61497993/how-to-insert-array-values-into-a-html-table-with-javascript
 function displayLibrary() {
-  // pass
+  var table = document.getElementById("datas");
+  table.innerHTML="";
+  var tr="";
+  myLibrary.forEach(book=>{
+     tr+='<tr>';
+     tr+='<td>'+book.title+'</td>'+'<td>'+book.author+'</td>'+'<td>'+book.pages+'</td>'+'<td>'+book.read_info(book.read)+'</td>'
+     tr+='</tr>'
+  })
+  table.innerHTML+=tr;
 }
 
-addBookToLibrary(theHobbit)
+addBookToLibrary(theHobbit);
+addBookToLibrary(greatGatsby);
+addBookToLibrary(warAndPeace);
+addBookToLibrary(mobyDick);
 
-console.log(theHobbit.info())
 
+displayLibrary(myLibrary)
